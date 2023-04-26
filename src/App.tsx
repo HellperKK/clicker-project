69676e0008f01fe7c093350118f81e05117e2017
Adrien Baudet <baudetad@gmail.com>
2023-04-26 19:38:21 +0200

    update apearance

--- src/App.tsx
+++ src/App.tsx
@@ -28,33 +28,39 @@ function App() {
         <div className="column is-one-quarter">
           <p>
             Monney : {money} (+{calculateBuildingReward(buildings)}/s)
-            <button onClick={() => dispatch(changeByAmount(1))}>Click</button>
           </p>
+          <button
+            className="button"
+            onClick={() => dispatch(changeByAmount(1))}
+          >
+            Click
+          </button>
         </div>
-        <div className="column"></div>
-        <div className="column is-one-quarter">
+        <div className="column">
           {buildings.map((building) => (
-            <HideElement
-              minimalShow={building.basePrice * 0.75}
-              key={building.id}
-            >
-              <button
-                className="button"
-                title={building.desc}
-                disabled={money < calculateBuildingPrice(building)}
-                onClick={() => {
-                  dispatch(
-                    changeByAmount(Math.ceil(-calculateBuildingPrice(building)))
-                  );
-                  dispatch(buyBuilding(building));
-                }}
-              >
-                {building.name}(+{building.moneyGain}/s){" "}
-                {calculateBuildingPrice(building)}
-              </button>
-            </HideElement>
+            <div key={building.id}>
+              <HideElement minimalShow={building.basePrice * 0.75}>
+                <button
+                  className="button"
+                  title={building.desc}
+                  disabled={money < calculateBuildingPrice(building)}
+                  onClick={() => {
+                    dispatch(
+                      changeByAmount(
+                        Math.ceil(-calculateBuildingPrice(building))
+                      )
+                    );
+                    dispatch(buyBuilding(building));
+                  }}
+                >
+                  {building.name}(+{building.moneyGain}/s){" "}
+                  {calculateBuildingPrice(building)}
+                </button>
+              </HideElement>
+            </div>
           ))}
         </div>
+        <div className="column is-one-quarter"></div>
       </div>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { store } from "./store";
 import { Provider } from "react-redux";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider store={store}>
