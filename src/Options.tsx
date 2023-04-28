8e5b0d716b79832fe9984a98098991af51b33dc5
Adrien Baudet <baudetad@gmail.com>
2023-04-28 19:10:38 +0200

    refacto

--- src/Options.tsx
+++ src/Options.tsx
@@ -2,41 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { resetMoney, setMoney } from "./moneySlice";
 import { resetBuildings, setBuildings } from "./buildingSlice";
 import { RootState } from "./store";
-
-function download(fileName: string, text: string) {
-  //creating an invisible element
-  const element = document.createElement("a");
-  element.setAttribute(
-    "href",
-    "data:text/plain;charset=utf-8, " + encodeURIComponent(text)
-  );
-  element.setAttribute("download", fileName);
-  element.click();
-}
-
-const openFiles = (
-  accept: Array<string> = [],
-  multiple = false
-): Promise<FileList> => {
-  return new Promise((resolve, reject) => {
-    const fileSelector = document.createElement("input");
-    fileSelector.type = "file";
-    fileSelector.multiple = multiple;
-
-    if (accept.length > 0) {
-      fileSelector.accept = accept.join(",");
-    }
-
-    fileSelector.addEventListener("change", () => {
-      if (fileSelector.files !== null) {
-        resolve(fileSelector.files);
-      } else {
-        reject(new Error("no file selected"));
-      }
-    });
-    fileSelector.click();
-  });
-};
+import { download, openFiles } from "./utils";
 
 function Options() {
   const dispatch = useDispatch();
