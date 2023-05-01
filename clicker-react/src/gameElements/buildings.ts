1c03f98e5f201b133007d3c986d22b29f78b280f
Adrien Baudet <baudetad@gmail.com>
2023-05-01 14:42:49 +0200

    try improving buildings

--- clicker-react/src/gameElements/buildings.ts
+++ clicker-react/src/gameElements/buildings.ts
@@ -8,6 +8,26 @@ export interface Building {
   isUnlocked: boolean;
 }
 
+const buildingMaker = {
+  id: 1,
+  make: function (
+    name: string,
+    desc: string,
+    basePrice: number,
+    moneyGain: number
+  ): Building {
+    return {
+      name,
+      desc,
+      basePrice,
+      moneyGain,
+      id: this.id++,
+      quantity: 0,
+      isUnlocked: false,
+    };
+  },
+};
+
 export function buildingsGain(buildings: Array<Building>) {
   return buildings.reduce((memo, building) => {
     return memo + building.moneyGain * building.quantity;
@@ -23,58 +43,20 @@ export function buildingShowingValue(building: Building) {
 }
 
 export const BUILDINGS: Array<Building> = [
-  {
-    id: 1,
-    name: "Finger",
-    desc: "A finger to click for you.",
-    basePrice: 10,
-    quantity: 0,
-    moneyGain: 1,
-    isUnlocked: false,
-  },
-  {
-    id: 2,
-    name: "Palm",
-    desc: "Many fingers at the same time.",
-    basePrice: 200,
-    quantity: 0,
-    moneyGain: 5,
-    isUnlocked: false,
-  },
-  {
-    id: 3,
-    name: "Fist",
-    desc: "A stronger hand by punching.",
-    basePrice: 3000,
-    quantity: 0,
-    moneyGain: 25,
-    isUnlocked: false,
-  },
-  {
-    id: 4,
-    name: "Chest",
-    desc: "To press with the whole body.",
-    basePrice: 50_000,
-    quantity: 0,
-    moneyGain: 200,
-    isUnlocked: false,
-  },
-  {
-    id: 5,
-    name: "Hammer",
-    desc: "When the need for tools start to rise.",
-    basePrice: 1_000_000,
-    quantity: 0,
-    moneyGain: 1000,
-    isUnlocked: false,
-  },
-  {
-    id: 6,
-    name: "Mass",
-    desc: "A heavier tool for more strenght.",
-    basePrice: 15_000_000,
-    quantity: 0,
-    moneyGain: 5000,
-    isUnlocked: false,
-  },
+  buildingMaker.make("Finger", "A finger to click for you.", 10, 1),
+  buildingMaker.make("Palm", "Many fingers at the same time.", 200, 5),
+  buildingMaker.make("Fist", "A stronger hand by punching.", 3000, 25),
+  buildingMaker.make("Chest", "To press with the whole body.", 50_000, 200),
+  buildingMaker.make(
+    "Hammer",
+    "When the need for tools start to rise.",
+    1_000_000,
+    1000
+  ),
+  buildingMaker.make(
+    "Mass",
+    "A heavier tool for more strenght.",
+    15_000_000,
+    5000
+  ),
 ];
