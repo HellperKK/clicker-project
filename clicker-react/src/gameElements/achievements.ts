6b4a2912d290da6c675d648afc7d95052546b3a2
Adrien Baudet <baudetad@gmail.com>
2023-05-01 17:11:04 +0200

    update achievements creation

--- clicker-react/src/gameElements/achievements.ts
+++ clicker-react/src/gameElements/achievements.ts
@@ -7,41 +7,51 @@ export interface Achievement {
   condition: (state: GameState) => boolean;
 }
 
+const achievementMaker = {
+  id: 1,
+  make: function (
+    name: string,
+    description: string,
+    condition: (state: GameState) => boolean
+  ): Achievement {
+    return {
+      name,
+      description,
+      condition,
+      id: this.id++,
+    };
+  },
+};
+
 export const ACHIEVEMENTS = [
-  {
-    id: 1,
-    name: "10 fingers",
-    description: "Purchase 10 fingers",
-    condition: (state: GameState) => state.buildings[0].quantity >= 10,
-  },
-  {
-    id: 2,
-    name: "50 fingers",
-    description: "Purchase 50 fingers",
-    condition: (state: GameState) => state.buildings[0].quantity >= 50,
-  },
-  {
-    id: 3,
-    name: "100 fingers",
-    description: "Purchase 100 fingers",
-    condition: (state: GameState) => state.buildings[0].quantity >= 100,
-  },
-  {
-    id: 4,
-    name: "10 palms",
-    description: "Purchase 10 palms",
-    condition: (state: GameState) => state.buildings[1].quantity >= 10,
-  },
-  {
-    id: 5,
-    name: "50 palms",
-    description: "Purchase 50 palms",
-    condition: (state: GameState) => state.buildings[1].quantity >= 50,
-  },
-  {
-    id: 6,
-    name: "100 palms",
-    description: "Purchase 100 palms",
-    condition: (state: GameState) => state.buildings[1].quantity >= 100,
-  },
+  achievementMaker.make(
+    "10 fingers",
+    "Purchase 10 fingers",
+    (state: GameState) => state.buildings[0].quantity >= 10
+  ),
+  achievementMaker.make(
+    "50 fingers",
+    "Purchase 50 fingers",
+    (state: GameState) => state.buildings[0].quantity >= 50
+  ),
+  achievementMaker.make(
+    "100 fingers",
+    "Purchase 100 fingers",
+    (state: GameState) => state.buildings[0].quantity >= 100
+  ),
+  achievementMaker.make(
+    "10 palms",
+    "Purchase 10 palms",
+    (state: GameState) => state.buildings[1].quantity >= 10
+  ),
+  achievementMaker.make(
+    "50 palms",
+    "Purchase 50 palms",
+    (state: GameState) => state.buildings[1].quantity >= 50
+  ),
+  achievementMaker.make(
+    "100 palms",
+    "Purchase 100 palms",
+    (state: GameState) => state.buildings[1].quantity >= 100
+  ),
 ];
