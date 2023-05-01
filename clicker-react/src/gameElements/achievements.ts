771a31783a36017a9a4dc0005f041f2da9984332
Adrien Baudet <baudetad@gmail.com>
2023-05-01 19:36:59 +0200

    add achivements

--- clicker-react/src/gameElements/achievements.ts
+++ clicker-react/src/gameElements/achievements.ts
@@ -54,4 +54,64 @@ export const ACHIEVEMENTS = [
     "Purchase 100 palms",
     (state: GameState) => state.buildings[1].quantity >= 100
   ),
+  achievementMaker.make(
+    "10 fists",
+    "Purchase 10 fists",
+    (state: GameState) => state.buildings[2].quantity >= 10
+  ),
+  achievementMaker.make(
+    "50 fists",
+    "Purchase 50 fists",
+    (state: GameState) => state.buildings[2].quantity >= 50
+  ),
+  achievementMaker.make(
+    "100 fists",
+    "Purchase 100 fists",
+    (state: GameState) => state.buildings[2].quantity >= 100
+  ),
+  achievementMaker.make(
+    "10 chests",
+    "Purchase 10 chests",
+    (state: GameState) => state.buildings[3].quantity >= 10
+  ),
+  achievementMaker.make(
+    "50 chests",
+    "Purchase 50 chests",
+    (state: GameState) => state.buildings[3].quantity >= 50
+  ),
+  achievementMaker.make(
+    "100 chests",
+    "Purchase 100 chests",
+    (state: GameState) => state.buildings[3].quantity >= 100
+  ),
+  achievementMaker.make(
+    "10 hammers",
+    "Purchase 10 hammers",
+    (state: GameState) => state.buildings[4].quantity >= 10
+  ),
+  achievementMaker.make(
+    "50 hammers",
+    "Purchase 50 hammers",
+    (state: GameState) => state.buildings[4].quantity >= 50
+  ),
+  achievementMaker.make(
+    "100 hammers",
+    "Purchase 100 hammers",
+    (state: GameState) => state.buildings[4].quantity >= 100
+  ),
+  achievementMaker.make(
+    "10 mass",
+    "Purchase 10 mass",
+    (state: GameState) => state.buildings[4].quantity >= 10
+  ),
+  achievementMaker.make(
+    "50 mass",
+    "Purchase 50 mass",
+    (state: GameState) => state.buildings[4].quantity >= 50
+  ),
+  achievementMaker.make(
+    "100 mass",
+    "Purchase 100 mass",
+    (state: GameState) => state.buildings[4].quantity >= 100
+  ),
 ];
